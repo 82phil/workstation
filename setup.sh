7c134b36b40e844b26ece1c3329d14dce6cabbc7
BuildTools <unconfigured@null.spigotmc.org>
2019-11-18 20:55:36 -0800

    Adding Vim and tmux config, organization

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 
+cp home/.vimrc ~
+cp home/.tmux.conf ~
 sudo apt-get install git
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
