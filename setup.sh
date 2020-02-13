21c3368b9f1acf41aebd6ace7eb2f75872e015f8
Philip Hoffman <philip.hoffman@metergroup.com>
2020-02-13 12:29:20 -0800

    Adding docker relay script, tmux resurrect plugin, more installs to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
+# Initial setup
+sudo apt-get update
 
 cp home/.vimrc ~
 cp home/.tmux.conf ~
+
+# Needed for a few tools
+sudo apt install golang-go
+
+# Install Vim Plugins
 sudo apt-get install git
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+
+# Install Docker Client
+sudo apt install socat docker.io
 
